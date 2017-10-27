47213b9305704e84f4dff79dd9663725649eaa15
ZhaoLv <lvzhao_gfkd@126.com>
2017-10-27 11:29:05 +0800

    加入reset信号在interface

--- interface.sv
+++ interface.sv
@@ -4,13 +4,14 @@ logic line1;
 logic line2;
 ligic outp;
 logic overflw;    
+logic reset;
 /*
 modport TEST(output line1,line2,
 						 input outp,overflw,clk) ;
 						 */
 modport b01
 
-modport DUT(input line1,line2, clk,   
+modport DUT(input line1,line2, clk,reset   
             output outp,overflw) ;
 modport MONITOR(input line1,line2, clk,outp,overflw);
                 
